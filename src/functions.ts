d9d6483b7def961cc9e30804cf1f5b43a7f8b2d5
Ike Ogbonnia <iike@udel.edu>
2026-08-29 13:14:35 -0400

    completed functions good refresher and I'm glad I could work through and solve my issues

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,7 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    return  (temperature - 32) * 5/9;
 }
 
 /**
@@ -12,7 +12,19 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    //need to find a way that only adds a number if it is positive if not, do not add
+
+    let sum = 0;
+    if(first > 0){
+        sum += first
+    }
+    if(second > 0){
+        sum += second
+    }
+     if(third > 0){
+        sum += third
+    }
+    return sum;
 }
 
 /**
@@ -20,7 +32,9 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
+
+
 }
 
 /**
@@ -28,7 +42,9 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+
+    return message.endsWith("?") ? true : false;
+    //or message.slice(-1);
 }
 
 /**
@@ -37,5 +53,16 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if(word.toUpperCase() == "YES" || word == "yes"){
+        return true;
+    }
+
+    else if(word.toUpperCase() == "NO" || word == "no"){
+        return false;
+    }
+
+    else{
+        return null;
+    }
+
 }
